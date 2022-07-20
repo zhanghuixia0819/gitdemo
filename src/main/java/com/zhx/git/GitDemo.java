d658843cfb65baf1eb84108eb32ff71a794bdeb6
zhanghuixia <zhanghuixia@163.com>
2022-07-20 17:18:50 +0800

    first Commit

---
+++ src/main/java/com/zhx/git/GitDemo.java
@@ -0,0 +1,12 @@
+package com.zhx.git;
+
+/**
+ * @Author: xiaoxiao
+ * @Date: 2022-07-20-11:35
+ * @Description:
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
---
+++ src/main/java/com/zhx/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zhx.git;
+
+/**
+ * @Author: xiaoxiao
+ * @Date: 2022-07-20-8:53
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("zhanghuixia");
+    }
+}
