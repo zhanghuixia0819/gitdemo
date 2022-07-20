0ba374b2727cf0fa2106b1f7d5da7f6ee10151e5
zhanghuixia <zhanghuixia@163.com>
2022-07-20 19:10:16 +0800

    second Commit

--- src/main/java/com/zhx/git/GitTest.java
+++ src/main/java/com/zhx/git/GitTest.java
@@ -8,6 +8,6 @@ package com.zhx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("zhanghuixia");
+        System.out.println("zhanghuixia!");
     }
 }
