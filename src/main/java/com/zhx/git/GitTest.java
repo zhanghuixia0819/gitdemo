779167ed72aa6b3aea0e087d8e4b2736d4fd31ea
zhanghuixia <zhanghuixia@163.com>
2022-07-23 08:32:25 +0800

    first Commit-firefox

--- src/main/java/com/zhx/git/GitTest.java
+++ src/main/java/com/zhx/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("zhanghuixia!");
+        System.out.println("dangzheng!");
     }
 }
